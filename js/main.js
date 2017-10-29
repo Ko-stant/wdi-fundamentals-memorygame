0940e5ebdfa83cc6bc1511be91850d68eeb23ebe
Ko-stant <kostant.stanton@gmail.com>
2017-10-29 18:23:06 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
 cards = ['queen', 'queen', 'king', 'king'];
 cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-console.log('User flipped ' + cardsInPlay);
+// console.log('User flipped ' + cardsInPlay);
 
-// check to see if cards in play is 2. cardsinplay.length - 1;
 
-if (cardsInPlay.length == 2) {
-	console.log("User has flipped two cards.");
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again!");
 	}
-} else if (cardsInPlay.length == 1) {
-	console.log("User needs to flip another card.");
-} else {
-	console.log("User needs to flip two cards.")
 }
+
+// check to see if cards in play is 2. return if not, check for match if yes
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length < 2) {
+		return;
+	} else {
+		checkForMatch();
+	}
+}
+
+
+flipCard(0);
+flipCard(2);
